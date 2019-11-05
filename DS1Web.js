f439aa5b928b2e11d4fc8c1a9de29faae4c9f2cc
Client3@EST.LAN <Client3@EST.LAN>
2019-11-05 15:41:46 +0000

    version 3

--- DS1Web.js
+++ DS1Web.js
@@ -1,19 +1,23 @@
+//JAHID KHADIJA ZAHIRA EL MAHDAOUY n_Poste : 04
 //Question 1 :
+var score_j_current =document.querySelector('#courent-0');
+var score_j_current2 =document.querySelector('#courent-1');
+var score_joueur1=document.querySelector('#score-0');
+var score_joueur2=document.querySelector('#score-1');
+var joueur2=document.querySelector('#nom-1');
+var joueur1=document.querySelector('#nom-0');
+var icon=document.querySelectorAll('.ion-social-tux');
+var de1=document.querySelector('.de');
+var bcj1=document.querySelector('.panel-joueur-0');
+var bcj2=document.querySelector('.panel-joueur-1');
 function init()
 {
-    var score_j_current =document.querySelector('#courent-0');
-    var score_j_current2 =document.querySelector('#courent-1');
-    var score_joueur1=document.querySelector('#score-0');
-    var score_joueur2=document.querySelector('#score-1');
     score_j_current.innerHTML=0;
     score_j_current2.innerHTML=0;
     score_joueur1.innerHTML=0;
     score_joueur2.innerHTML=0;
-    var de=document.querySelector('.de');
-    de.setAttribute('hidden','');
-    var joueur2=document.querySelector('#nom-1');
+    de1.setAttribute('hidden','');
     joueur2.style.color='#C0C0C0';
-    var icon=document.querySelectorAll('.ion-social-tux');
     icon[1].style.color='#C0C0C0';
     icon[0].style.color='green';
 }
@@ -21,11 +25,10 @@ init();
 //Question 2 :
 const lancer=document.querySelector('.btn-lancer');
 var i=0;
+var score1=document.querySelector('#score-0');
 function lance()
 {
 	var tempScore=document.querySelector('#courent-0');
-	var de1=document.querySelector('.de');
-	var score1=document.querySelector('#score-0');
 	var nombre=Math.floor(Math.random() * ( 6 )+1);
 	console.log(nombre);
 	de1.removeAttribute('hidden','');
@@ -46,13 +49,25 @@ if(nombre!=1)
 	else
 	{
 		tempScore.innerHTML=0;
-		de1.setAttribute('hidden','');
-		score1.innerHTML=i;
 	}
 
 }
 lancer.addEventListener('click',lance);
-/*function joueurSuivant()
+//Question 3 :
+function joueurSuivant()
 {
+		de1.setAttribute('hidden','');
+		score1.innerHTML=i;
+		score_j_current.innerHTML=0;
+		score_j_current2.innerHTML=0;
+		score_joueur2.innerHTML=0;
+		joueur2.style.color='black';
+		joueur1.style.color='#C0C0C0';
+		icon[0].style.color='#C0C0C0';
+    	icon[1].style.color='green';
+    	bcj1.backgroundColor='gray';
+    	bcj2.backgroundColor='white';
 
-}*/
+}
+var passer =document.querySelector('.btn-passe');
+passer.addEventListener('click',joueurSuivant);
